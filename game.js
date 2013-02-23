37c5b4f6094058b57e8987bacc2a97c48b87a3de
daniel <daniel@freshmilk.tv>
2013-02-23 19:44:41 +0100

    Remove some useless bytes and fix bug in random enemy positioning

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 var r = function (i, j) {
-    return Math.floor((Math.random() * j) + i);
+    return Math.floor((Math.random() * (j - i)) + i);
 };
 
 var N = function () {
-    return [r(s, W - (s * 2)), r(s, H - (s * 2)), 9];
+    return [r(s * 3, W - (s * 2)), r(s, H - (s * 2)), 9];
 };
 
 var I = window,
@@ -12,7 +12,7 @@ var I = window,
     s = 32, // size
     t = 9, // tempo
     B = [], // bullet
-    Bs = 6, // Bullet size
+    b = 6, // Bullet size
     E = N(), // Enemy
     P = [s / 2, (H / 2) - (s / 2)], // x, y
     K = 0, // KILLS
@@ -42,7 +42,7 @@ I.onkeydown = function (e) {
     }
 
     if (C == 32) { // fire
-        B.push([P[0] + s, P[1] + (s / 2) - (Bs / 2)]);
+        B.push([P[0] + s, P[1] + (s / 2) - (b / 2)]);
     }
 
     // collision
@@ -60,6 +60,7 @@ var M = I.setInterval(function () {
     // draw map
     a.clearRect(0, 0, W, H);
     R(0, 0, W, H, '#cc6');
+    R(576, 416, s, s, '#f00');
     R(s * 2, 0, 2, H, '#993');
 
     // enemy
@@ -69,7 +70,7 @@ var M = I.setInterval(function () {
     for (var i = 0; i < B.length; i++) {
         B[i][0] += 2;
 
-        if (B[i][0] <= (W - (Bs * 2))) { // end of the field
+        if (B[i][0] <= (W - (b * 2))) { // end of the field
             var A = B[i][0] >= E[0];
             var Z = B[i][1] > E[1] && B[i][1] < (E[1] + s);
 
@@ -77,14 +78,14 @@ var M = I.setInterval(function () {
                 B.splice(i, 1);
                 if (E[2] > 0) {
                     R(E[0], E[1], s, s, '#f0' + E[2]);
-                    E[2] -= 1;
+                    E[2]--;
                 } else {
                     K++;
                     E = N();
                     R(E[0], E[1], s, s, '#f0' + E[2]);
                 }
             } else {
-                R(B[i][0], B[i][1], Bs, Bs, '#ff0');
+                R(B[i][0], B[i][1], b, b, '#ff0');
             }
         }
     }
