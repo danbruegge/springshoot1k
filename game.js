3e3c4e8c49e5873cd704b0b4e489e1a13f5364e0
daniel <daniel@freshmilk.tv>
2013-02-23 16:14:37 +0100

    theoretical complete

--- game.js
+++ game.js
@@ -1,93 +1,112 @@
-window.onload = function () {
-    'use strict';
+var r = function (i, j) {
+    return Math.floor((Math.random() * j) + i);
+};
 
-    var r = function (i, j) {
-        return Math.floor((Math.random()*j)+i);
+var N = function () {
+    return [r(s, W - (s * 2)), r(s, H - (s * 2)), 9];
+};
+
+var W = 640,
+    H = 480,
+    s = 32, // size
+    t = 9, // tempo
+    B = [], // bullet
+    Bs = 6, // Bullet size
+    E = N(), // Enemy
+    P = [s / 2, (H / 2) - (s / 2)], // x, y
+    K = 0, // KILLS
+    T = 10, // Time to count down
+    h = 0; // time helper
+
+a.canvas.width = W;
+a.canvas.height = H;
+
+var rect = function (x, y, w, h, color) {
+    a.beginPath();
+    a.rect(x, y, w, h);
+    a.fillStyle = color;
+    a.closePath();
+    a.fill();
+};
+
+// Bindings
+window.onkeydown = function (e) {
+    var C = e.keyCode;
+    if (C === 38) { // up
+        P[1] -= t;
     }
 
-    var N = function () {
-        return [r(s, W - (s * 2)), r(s, H - (s * 2)), 9];
+    if (C === 40) { // down
+        P[1] += t;
     }
 
-    var W = 640,
-        H = 480,
-        s = 32, // size
-        t = 9, // tempo
-        B = [], // bullet
-        Bs = 6,
-        E = N(),
-        P = [s / 2, (H / 2) - (s / 2)]; // x, y
+    if (C === 32) { // fire
+        B.push([P[0] + s, P[1] + (s / 2) - (Bs / 2)]);
+    }
 
-    a.canvas.width = W;
-    a.canvas.height = H;
+    // collision
+    if (P[1] <= 0) {
+        P[1] = 0;
+    }
 
-    var rect = function (x, y, w, h, color) {
-        a.beginPath();
-        a.rect(x, y, w, h);
-        a.fillStyle = color;
-        a.closePath();
-        a.fill();
-    };
+    if (P[1] >= (H - s)) {
+        P[1] = (H - s);
+    }
+};
 
-    // Bindings
-    window.onkeydown = function (e) {
-        var C = e.keyCode;
-        if (C === 38) { // up
-            P[1] -= t;
-        }
+// main();
+var main = window.setInterval(function () {
+    // draw map
+    a.clearRect(0, 0, W, H);
+    rect(0, 0, W, H, '#cc6');
+    rect(s * 2, 0, 2, H, '#993');
 
-        if (C === 40) { // down
-            P[1] += t;
-        }
+    // enemy
+    rect(E[0], E[1], s, s, '#f0' + E[2]);
 
-        if (C === 32) { // fire
-            B.push([P[0] + s, P[1] + (s / 2) - (Bs / 2)]);
-        }
+    // bullets
+    for (var i = 0; i < B.length; i++) {
+        B[i][0] += 2;
 
-        // collision
-        if (P[1] <= 0) {
-            P[1] = 0;
-        }
+        if (B[i][0] <= (W - (Bs * 2))) { // end of the field
+            var A = B[i][0] >= E[0];
+            var Z = B[i][1] > E[1] && B[i][1] < (E[1] + s);
 
-        if (P[1] >= (H - s)) {
-            P[1] = (H - s);
-        }
-    };
-
-    // main();
-    window.setInterval(function () {
-        // draw map
-        a.clearRect(0, 0, W, H);
-        rect(0, 0, W, H, '#cc6');
-        rect(s * 2, 0, 2, H, '#993');
-
-        // enemy
-        rect(E[0], E[1], s, s, '#f0' + E[2]);
-
-        // bullets
-        for (var i = 0; i < B.length; i++) {
-            B[i][0] += 2;
-
-            if (B[i][0] <= (W - (Bs * 2))) { // end of the field
-                var A = B[i][0] >= E[0];
-                var Z = B[i][1] > E[1] && B[i][1] < (E[1] + s);
-
-                if (A && Z) { // enemy collision
-                    B.splice(i, 1);
-                    if (E[2] > 0) {
-                        rect(E[0], E[1], s, s, '#f0' + E[2]);
-                        E[2] -= 1;
-                    } else {
-                        E = N();
-                        rect(E[0], E[1], s, s, '#f0' + E[2]);
-                    }
+            if (A && Z) { // enemy collision
+                B.splice(i, 1);
+                if (E[2] > 0) {
+                    rect(E[0], E[1], s, s, '#f0' + E[2]);
+                    E[2] -= 1;
                 } else {
-                    rect(B[i][0], B[i][1], Bs, Bs, '#ff0');
+                    K++;
+                    E = N();
+                    rect(E[0], E[1], s, s, '#f0' + E[2]);
                 }
+            } else {
+                rect(B[i][0], B[i][1], Bs, Bs, '#ff0');
             }
         }
+    }
 
-         // draw player
-        rect(P[0], P[1], s, s, '#693');
-    }, 10);
-};
+    // draw player
+    rect(P[0], P[1], s, s, '#693');
+
+    // draw infos
+    a.fillText('T: ' + T, 80, 10);
+    a.fillText('K: ' + K, 80, 20);
+
+    // count time down
+    h++;
+    if (h === 100) {
+        T--;
+        h = 0;
+    }
+
+    if (T === -1) {
+        window.clearInterval(main);
+
+        a.fillStyle = '#000';
+        a.textAlign = 'center';
+        a.fillText('F5 RESTART', W / 2, H / 2);
+    }
+}, 10);
